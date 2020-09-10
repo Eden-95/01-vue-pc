376baa990cfa64f029eb508d1e059876f738fd09
Eden-95 <954537718@qq.com>
2020-09-10 16:31:18 +0800

    登录页面样式基本完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 同步的方式
+// import Index from 'Index.vue'
+// 异步的方式
+const Index = () => import('../views/Index.vue')
+const Login = () => import('../views/Login.vue')
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/', component: Index, name: 'index' },
+  { path: '/login', component: Login, name: 'login' }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login">
+    <!--
+    :model="form" : 用于收集表单数据，是一个对象
+    label-width="80px" :label的宽度
+    -->
+    <el-form class="form" :model="user" label-width="40px">
+      <h2>后台管理系统</h2>
+      <el-form-item label="账号" class="zhanghao">
+        <el-input
+          v-model="user.username"
+          clearable
+          placeholder="请输入用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" class="mima">
+        <el-input
+          type="password"
+          v-model="user.password"
+          placeholder="请输入密码"
+          show-password
+        ></el-input>
+      </el-form-item>
+      <el-form-item class="anniu">
+        <el-button type="success">登录</el-button>
+        <el-button type="danger">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  .form {
+    width: 400px;
+    height: 240px;
+    background-color: #eee;
+    padding: 30px;
+    h2 {
+      text-align: center;
+    }
+    .zhanghao {
+      margin-top: 30px;
+    }
+    .mima {
+      margin-top: 20px;
+    }
+    .anniu {
+      margin-top: 30px;
+    }
+  }
+}
+</style>
